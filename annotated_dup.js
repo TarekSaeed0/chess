0eff0eaec95de750328a65fab735ca7f1b1681d0
TarekSaeed0 <TarekSaeed0@users.noreply.github.com>
2025-05-23 21:59:40 +0000

    deploy: f086ed2e6b69a2dc2238fbd2b251fd40d7e1317f

--- annotated_dup.js
+++ annotated_dup.js
@@ -1,5 +1,9 @@
 var annotated_dup =
 [
+    [ "attacks", null, [
+      [ "ChessOffset", "classattacks_1_1ChessOffset.html", null ],
+      [ "ChessPieceType", "classattacks_1_1ChessPieceType.html", null ]
+    ] ],
     [ "chess_move", "structchess__move.html", null ],
     [ "chess_moves", "structchess__moves.html", null ],
     [ "chess_position", "structchess__position.html", null ]
--- search/all_0.js
+++ search/all_0.js
@@ -3,5 +3,7 @@ var searchData=
   ['chess_0',['Chess',['../index.html',1,'']]],
   ['chess_5fmove_1',['chess_move',['../structchess__move.html',1,'']]],
   ['chess_5fmoves_2',['chess_moves',['../structchess__moves.html',1,'']]],
-  ['chess_5fposition_3',['chess_position',['../structchess__position.html',1,'']]]
+  ['chess_5fposition_3',['chess_position',['../structchess__position.html',1,'']]],
+  ['chessoffset_4',['ChessOffset',['../classattacks_1_1ChessOffset.html',1,'attacks']]],
+  ['chesspiecetype_5',['ChessPieceType',['../classattacks_1_1ChessPieceType.html',1,'attacks']]]
 ];
