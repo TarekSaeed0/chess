da81457171601b84c9bde2eecddad53adc92536d
TarekSaeed0 <TarekSaeed0@users.noreply.github.com>
2025-05-26 15:58:32 +0000

    deploy: 3e560a45aebd494a5ab508cc6d7bd992bf0d0aff

--- search/classes_0.js
+++ search/classes_0.js
@@ -2,7 +2,5 @@ var searchData=
 [
   ['chess_5fmove_0',['chess_move',['../structchess__move.html',1,'']]],
   ['chess_5fmoves_1',['chess_moves',['../structchess__moves.html',1,'']]],
-  ['chess_5fposition_2',['chess_position',['../structchess__position.html',1,'']]],
-  ['chessoffset_3',['ChessOffset',['../classattacks_1_1ChessOffset.html',1,'attacks']]],
-  ['chesspiecetype_4',['ChessPieceType',['../classattacks_1_1ChessPieceType.html',1,'attacks']]]
+  ['chess_5fposition_2',['chess_position',['../structchess__position.html',1,'']]]
 ];
