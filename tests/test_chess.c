41cef964b9dbd226d54f6974b3850f30b1680714
TarekSaeed0 <naminode2020@gmail.com>
2025-05-29 13:17:38 +0300

    tests: add test for `chess_move_is_valid`

--- tests/test_chess.c
+++ tests/test_chess.c
@@ -985,6 +985,82 @@ static void test_chess_square_to_algebraic(void **state) {
 	}
 }
 
+static void test_chess_move_is_valid(void **state) {
+	(void)state;
+
+	static CHESS_CONSTEXPR struct {
+		struct chess_move move;
+		bool is_valid;
+	} test_cases[] = {
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_NONE,
+		        .to             = CHESS_SQUARE_A2,
+		        .promotion_type = CHESS_PIECE_TYPE_NONE,
+		    },
+		    .is_valid = false,
+		},
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_A2,
+		        .to             = CHESS_SQUARE_NONE,
+		        .promotion_type = CHESS_PIECE_TYPE_NONE,
+		    },
+		    .is_valid = false,
+		},
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_A2,
+		        .to             = CHESS_SQUARE_A1,
+		        .promotion_type = CHESS_PIECE_TYPE_KING,
+		    },
+		    .is_valid = false,
+		},
+
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_A2,
+		        .to             = CHESS_SQUARE_A1,
+		        .promotion_type = CHESS_PIECE_TYPE_KNIGHT,
+		    },
+		    .is_valid = true,
+		},
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_B1,
+		        .to             = CHESS_SQUARE_C3,
+		        .promotion_type = CHESS_PIECE_TYPE_NONE,
+		    },
+		    .is_valid = true,
+		},
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_E2,
+		        .to             = CHESS_SQUARE_E4,
+		        .promotion_type = CHESS_PIECE_TYPE_NONE,
+		    },
+		    .is_valid = true,
+		},
+		{
+		    .move = (struct chess_move){
+		        .from           = CHESS_SQUARE_C7,
+		        .to             = CHESS_SQUARE_C8,
+		        .promotion_type = CHESS_PIECE_TYPE_QUEEN,
+		    },
+		    .is_valid = true,
+		}
+	};
+
+	for (size_t i = 0; i < ARRAY_LENGTH(test_cases); i++) {
+		bool is_valid = chess_move_is_valid(test_cases[i].move);
+		if (test_cases[i].is_valid) {
+			assert_true(is_valid);
+		} else {
+			assert_false(is_valid);
+		}
+	}
+}
+
 static void test_chess_preft(void **state) {
 	(void)state;
 
@@ -1031,6 +1107,8 @@ int main(void) {
 		cmocka_unit_test(test_chess_square_is_valid),
 		cmocka_unit_test(test_chess_square_from_algebraic),
 		cmocka_unit_test(test_chess_square_to_algebraic),
+
+		cmocka_unit_test(test_chess_move_is_valid),
 
 		cmocka_unit_test(test_chess_preft),
 	};
