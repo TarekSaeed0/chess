a854f1e43731933e9ddd9988134dffb1070a7fae
TarekSaeed0 <TarekSaeed0@users.noreply.github.com>
2025-10-10 19:36:28 +0000

    deploy: b5d41ccfbf22b506758c7bce43bf008834958e71

--- search/all_a.js
+++ search/all_a.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['piece_2eh_0',['piece.h',['../piece_8h.html',1,'']]],
   ['piece_5fcounts_1',['piece_counts',['../structChessPosition.html#a70521db0af985c968d7fd607b44070a9',1,'ChessPosition']]],
-  ['piece_5ftype_2eh_2',['piece_type.h',['../piece__type_8h.html',1,'']]],
-  ['pieces_3',['pieces',['../structChessPosition.html#afaeb6d8b095194b3bf47b99ed2a62a97',1,'ChessPosition']]],
-  ['position_2eh_4',['position.h',['../position_8h.html',1,'']]],
-  ['position_5fcounter_5',['position_counter',['../structChessPosition.html#a770ccc59423e549f4c835d433cf78036',1,'ChessPosition']]],
-  ['position_5fcounter_2eh_6',['position_counter.h',['../position__counter_8h.html',1,'']]],
-  ['previous_5fcastling_5frights_7',['previous_castling_rights',['../structChessMove.html#a89f9af5e74f37d7478c2e661e004d02c',1,'ChessMove']]],
-  ['previous_5fen_5fpassant_5fsquare_8',['previous_en_passant_square',['../structChessMove.html#a5652f6509f0d9074d4d599c599c56b73',1,'ChessMove']]],
-  ['previous_5fhalf_5fmove_5fclock_9',['previous_half_move_clock',['../structChessMove.html#ab02f841f8c4eef3c881e6e880c72194f',1,'ChessMove']]],
-  ['promotion_5ftype_10',['promotion_type',['../structChessMove.html#a12bb680310c81ef4ed7cd26783f61ffe',1,'ChessMove']]]
+  ['piece_5findices_2',['piece_indices',['../structChessPosition.html#a908c8ff2e3169a1b9a9576f36dd383ea',1,'ChessPosition']]],
+  ['piece_5ftype_2eh_3',['piece_type.h',['../piece__type_8h.html',1,'']]],
+  ['pieces_4',['pieces',['../structChessPosition.html#afaeb6d8b095194b3bf47b99ed2a62a97',1,'ChessPosition']]],
+  ['position_2eh_5',['position.h',['../position_8h.html',1,'']]],
+  ['position_5fcounter_6',['position_counter',['../structChessPosition.html#a770ccc59423e549f4c835d433cf78036',1,'ChessPosition']]],
+  ['position_5fcounter_2eh_7',['position_counter.h',['../position__counter_8h.html',1,'']]],
+  ['previous_5fcastling_5frights_8',['previous_castling_rights',['../structChessMove.html#a89f9af5e74f37d7478c2e661e004d02c',1,'ChessMove']]],
+  ['previous_5fen_5fpassant_5fsquare_9',['previous_en_passant_square',['../structChessMove.html#a5652f6509f0d9074d4d599c599c56b73',1,'ChessMove']]],
+  ['previous_5fhalf_5fmove_5fclock_10',['previous_half_move_clock',['../structChessMove.html#ab02f841f8c4eef3c881e6e880c72194f',1,'ChessMove']]],
+  ['promotion_5ftype_11',['promotion_type',['../structChessMove.html#a12bb680310c81ef4ed7cd26783f61ffe',1,'ChessMove']]]
 ];
--- structChessPosition.js
+++ structChessPosition.js
@@ -6,6 +6,7 @@ var structChessPosition =
     [ "full_move_number", "structChessPosition.html#a69935229252d01288bdb10bd11974539", null ],
     [ "half_move_clock", "structChessPosition.html#a925cf995629fcd8b16d32081b9675bc1", null ],
     [ "piece_counts", "structChessPosition.html#a70521db0af985c968d7fd607b44070a9", null ],
+    [ "piece_indices", "structChessPosition.html#a908c8ff2e3169a1b9a9576f36dd383ea", null ],
     [ "pieces", "structChessPosition.html#afaeb6d8b095194b3bf47b99ed2a62a97", null ],
     [ "position_counter", "structChessPosition.html#a770ccc59423e549f4c835d433cf78036", null ],
     [ "side_to_move", "structChessPosition.html#ac3327b502d8c7d9540f323a0c8a30ff6", null ]
